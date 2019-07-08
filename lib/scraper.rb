2331ffae8509103e94a567e201ba9db679687502
Daniel Bovington <daniel.bovington@flatironschool.com>
2019-07-08 16:23:36 +0100

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,11 +1,31 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 require_relative './course.rb'
 
 class Scraper
-  
-    def print_courses
+
+  def get_page
+    html = open('http://learn-co-curriculum.github.io/site-for-scraping/courses')
+    Nokogiri::HTML(html)
+  end
+
+  def get_courses
+    get_page.css('article.post')
+  end
+
+  def make_courses
+    records = get_courses
+    records.each do |record|
+      course = Course.new
+      course.title = record.css('h2').text
+      course.schedule = record.css('em.date').text
+      course.description = record.css('p').text
+    end
+  end
+
+  def print_courses
     self.make_courses
     Course.all.each do |course|
       if course.title
@@ -15,8 +35,4 @@ class Scraper
       end
     end
   end
-  
 end
-
-
-
